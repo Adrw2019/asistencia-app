35b68ce6edbcc421c161dbd551aa0d7654a0a42a
repararpc2024-cell <repararpc2024@gmail.com>
2026-07-10 21:30:34 -0500

    chore: remove password fix backdoor

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,5 +3,4 @@ const router = express.Router();
 const auth = require('../controllers/authController');
 router.post('/register', auth.register);
 router.post('/login', auth.login);
-router.get('/fix', auth.fix);
 module.exports = router;
